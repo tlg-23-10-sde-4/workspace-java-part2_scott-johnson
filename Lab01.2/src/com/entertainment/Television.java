217205301025364cb5a54627529c44cf6ff252b2
Scott Johnson <scott.johnson@tlgcohort.com>
2023-11-01 12:02:26 -0400

    Lab02.1 refine equals()

--- Lab01.2/src/com/entertainment/Television.java
+++ Lab01.2/src/com/entertainment/Television.java
@@ -44,6 +44,16 @@ public class Television {
     public void setVolume(int volume) {
         this.volume = volume;
     }
+/*
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Television that = (Television) obj;
+        return this.getVolume() == that.getVolume() && Objects.equals(this.getBrand(), that.getBrand());
+    }
+ */
 
     @Override
     public int hashCode() {
@@ -51,20 +61,17 @@ public class Television {
         *This is a poorly written hash function, because it easily yields "hash collisions."
         * a hash collision is when "different" object have the same hash code (just by coincidence)
         */
-       // return getBrand().length() + getVolume();  < bad
+       //return getBrand().length() + getVolume(); <===BAD
         // instead we rely on Objects.hash() to give us a "scientifically correct" hash function
         return Objects.hash(getBrand(), getVolume());
-
-
     }
-
 
 
     @Override
     public boolean equals(Object obj) {
        boolean result = false;
        // proceed only if 'obj' is really referencing a Television object
-       if(obj instanceof Television) {
+       if(obj != null && this.getClass() == obj.getClass()) {
            //safely downcast 'obj' to more specific reference type Television
            Television other = (Television) obj;
 
@@ -73,7 +80,6 @@ public class Television {
                    this.getVolume() == other.getVolume();
 
        }
-
        return result;
     }
 
--- Lab01.2/src/com/entertainment/client/TelevisionClient.java
+++ Lab01.2/src/com/entertainment/client/TelevisionClient.java
@@ -10,20 +10,13 @@ class TelevisionClient {
     public static void main(String[] args) {
         // show behavior of == versus equals()
         Television tvA = new Television("Sony", 50);
-        Television tvB = new Television("LG",   52);
+        Television tvB = new Television("Sony", 50);
         System.out.println();
 
 
         System.out.println("tvA ==tvB: " +  (tvA == tvB));
         System.out.println("tvA.equals(tvB): " + tvA.equals(tvB));
 
-//        System.out.println(tvA.hashCode());
-//
-//        System.out.println();
-//
-//        System.out.println(tvB.hashCode());
-//
-//        System.out.println();
 
         Set<Television> tvs = new HashSet<>();
         tvs.add(tvA);
