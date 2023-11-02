cb336761f3c334a7b0fd1c4169ef52a4bdbaf935
Scott Johnson <scott.johnson@tlgcohort.com>
2023-11-02 11:29:27 -0400

    Lab02,1 (JUnit getting started) complete

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,51 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    // fixture: this is the business object or objects under test
+    private Calculator calc;
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+
+    @BeforeClass
+    public static void initializeTestRun() {
+        System.out.println("BeforeClass initializeTestRun()");
+    }
+
+    @AfterClass
+    public static void finalizeTestRun() {
+        System.out.println("AfterClass finalizeTestRun()");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp()");
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
+
+    @Test
+    public void testAdd() {
+        System.out.println("Tests add()");
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+
+    @Test
+    public void testDivide() {
+        System.out.println("Tests divide()");
+        assertEquals(2.5, calc.divide(5, 2), .001);
+    }
+
+    @Test
+    public void testIsEven() {
+        System.out.println("Tests isEven()");
+        assertTrue(calc.isEven(10));
+        assertFalse(calc.isEven(11));
+    }
 }
