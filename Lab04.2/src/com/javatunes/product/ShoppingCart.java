497a9eb8d1afdd995a7022bcaa34de91ec02368f
Scott Johnson <scott.johnson@tlgcohort.com>
2023-11-03 18:41:02 -0400

    Lab04.2 Collections ? of anything extends

--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -11,16 +11,37 @@ package com.javatunes.product;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+// at runtime this will be ShoppingCart<MusicItem>
+// or ShoppingCart<MediaPlayer>
+// or ShoppingCart<Product>
 
-public class ShoppingCart<T> {
-  // storage for the cart's contents
-  private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
-  
-  public void addItem(T item) {
-    items.add(item);
-  }
-  
-  public void removeItem(T item) {
-    items.remove(item);
-  }
+public class ShoppingCart<T extends Product> {
+    // storage for the cart's contents
+    private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+
+    public double total() {
+        double result = 0.0;
+
+        for (T item : items) {
+            result += item.getPrice();
+        }
+        return result;
+    }
+
+    public Collection<T> allItems() {
+        return Collections.unmodifiableCollection(items);
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
 }
--- Lab04.2/test/com/javatunes/product/OrderTest.java
+++ Lab04.2/test/com/javatunes/product/OrderTest.java
@@ -9,37 +9,50 @@
 package com.javatunes.product;
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class OrderTest {
-  private ShoppingCart<Product> productCart;
-  private ShoppingCart<MusicItem> musicCart;
-  private ShoppingCart<MediaPlayer> mediaCart;
-  
-  @Before
-  public void init() {
-    productCart = new ShoppingCart<Product>();
-    productCart.addItem(new MusicItem("CD-501"));
-    productCart.addItem(new MediaPlayer("MP3-LP150"));
-    
-    musicCart = new ShoppingCart<MusicItem>();
-    musicCart.addItem(new MusicItem("CD-521"));
-    musicCart.addItem(new MusicItem("CD-514"));
-    
-    mediaCart = new ShoppingCart<MediaPlayer>();
-    mediaCart.addItem(new MediaPlayer("AAC-PL233"));
-  }
-  
-  /**
-   * TODO: create new Order for each cart above and pass cart to processOrder().
-   *  For example, create three Order objects: "order-1", "order-2", "order-3."
-   *  order-1 is for the contents of productCart,
-   *  order-2 is for the contents of musicCart,
-   *  order-3 is for the contents of meduaCart.
-   */
-  @Test
-  public void testProcessCart() {
-    
-  }
+    private ShoppingCart<Product> productCart;
+    private ShoppingCart<MusicItem> musicCart;
+    private ShoppingCart<MediaPlayer> mediaCart;
+
+    @Before
+    public void init() {
+        productCart = new ShoppingCart<Product>();
+        productCart.addItem(new MusicItem("CD-501"));
+        productCart.addItem(new MediaPlayer("MP3-LP150"));
+
+        musicCart = new ShoppingCart<MusicItem>();
+        musicCart.addItem(new MusicItem("CD-521"));
+        musicCart.addItem(new MusicItem("CD-514"));
+
+        mediaCart = new ShoppingCart<MediaPlayer>();
+        mediaCart.addItem(new MediaPlayer("AAC-PL233"));
+    }
+
+    /**
+     * TODO: create new Order for each cart above and pass cart to processOrder().
+     *  For example, create three Order objects: "order-1", "order-2", "order-3."
+     *  order-1 is for the contents of productCart,
+     *  order-2 is for the contents of musicCart,
+     *  order-3 is for the contents of mediaCart.
+     */
+    @Test
+    public void testProcessCart() {
+        Order order1 = new Order("order-1");  // this is a ShoppingCart<Product>
+        order1.processCart(productCart);
+        System.out.println();
+
+        Order order2 = new Order("order-2");
+        order2.processCart(musicCart);  // this is a ShoppingCart<MusicItem>
+        System.out.println();
+
+        Order order3 = new Order("order-3");
+        order3.processCart(mediaCart);  // this is a ShoppingCart<MediaPlayer>
+        System.out.println();
+
+
+    }
 }
