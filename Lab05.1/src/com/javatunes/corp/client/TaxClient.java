58549cc7bde45cd2ca917c2443e2f8cfebf982aa
Scott Johnson <scott.johnson@tlgcohort.com>
2023-11-06 15:11:23 -0500

    Lab05.1 Singleton (ISEnum and IRS interface)

---
+++ Lab05.1/src/com/javatunes/corp/client/TaxClient.java
@@ -0,0 +1,33 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+
+package com.javatunes.corp.client;
+
+import com.javatunes.corp.Corporation;
+import gov.irs.IRS;
+
+class TaxClient {
+
+    public static void main(String[] args) {
+        // get the IRS instance via static factory method this is my only choice from here
+         IRS irs = IRS.getInstance();
+
+        // TODO: uncomment this block after completing the above TODO
+        // create the Corporation and register it with the IRS
+
+        Corporation corp = new Corporation("JavaTunes");
+        irs.register(corp);
+
+        // IRS collects taxes
+
+
+        System.out.println("IRS collects taxes");
+        irs.collectTaxes();
+
+    }
+}
---
+++ Lab05.1/src/gov/irs/IRS.java
@@ -0,0 +1,13 @@
+package gov.irs;
+
+public interface IRS {
+
+
+    public void collectTaxes();
+    public void register(TaxPayer payer);
+
+    // static factory method to obtain the instance
+    public static IRS getInstance() {
+        return IRSEnum.INSTANCE;
+    }
+}
