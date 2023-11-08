b502067b9eb99233fe10ef24768028f7cb51740e
Scott Johnson <scott.johnson@tlgcohort.com>
2023-11-08 15:18:07 -0500

    Lab03.2 EmployeeTest with anonymous subclass

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -14,24 +14,54 @@ public class EmployeeTest {
 
     @Before
     public void setUp() {
-        emp1 = new SalariedEmployee("Mary", Date.valueOf("2000-02-02"));
-        emp2 = new SalariedEmployee("Mary", Date.valueOf("2000-02-02"));
+        //emp1 = new DummyEmployee("Mary", Date.valueOf("2000-02-02"));
+        //emp2 = new DummyEmployee("Mary", Date.valueOf("2000-02-02"));
+        emp1 = getEmployee();
+        emp2 = getEmployee();
+
     }
 
-    @Test
-    public void equals_shouldReturnFalse_differentName_sameHireDate() {
-        emp2.setName("Joey");
-        assertNotEquals(emp1, emp2);
-    }
+    private Employee getEmployee() {
+        return new Employee("Mary", Date.valueOf("2000-02-02")) {
+            public double pay() {
+                return 0;
+            }
 
-    @Test
-    public void equals_shouldReturnFalse_sameName_differentHireDate() {
-        emp2.setHireDate(Date.valueOf("1999-09-09"));
-        assertNotEquals(emp1, emp2);
+            public double payTaxes() {
+                return 0;
+            }
+        };
     }
+        @Test
+        public void equals_shouldReturnFalse_differentName_sameHireDate () {
+            emp2.setName("Joey");
+            assertNotEquals(emp1, emp2);
+        }
 
-    @Test
-    public void equals_shouldReturnTrue_allPropertiesSame() {
-        assertEquals(emp1, emp2);
+        @Test
+        public void equals_shouldReturnFalse_sameName_differentHireDate () {
+            emp2.setHireDate(Date.valueOf("1999-09-09"));
+            assertNotEquals(emp1, emp2);
+        }
+
+        @Test
+        public void equals_shouldReturnTrue_allPropertiesSame () {
+            assertEquals(emp1, emp2);
+        }
+
+        //NAMES MEMBER LEVEL INNER CLASSES
+        private class DummyEmployee extends Employee {
+            public DummyEmployee(String name, Date hireDate) {
+                super(name, hireDate);
+            }
+
+            // done on one line becuase we dont need these 2 methods to change. but are required
+            public double pay() {
+                return 0;
+            }
+
+            public double payTaxes() {
+                return 0;
+            }
+        }
     }
-}
