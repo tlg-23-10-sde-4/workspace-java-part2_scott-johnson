21af8946a637d2dd724984742121b74355876123
Scott Johnson <scott.johnson@tlgcohort.com>
2023-11-13 13:33:11 -0500

    Lab11.1A extending Thread

--- Lab11.1A/src/com/javatunes/thread/MessagePrinterClient.java
+++ Lab11.1A/src/com/javatunes/thread/MessagePrinterClient.java
@@ -9,7 +9,23 @@ package com.javatunes.thread;
 
 public class MessagePrinterClient {
 
-  public static void main(String[] args) {
-    // TODO: create two instances of your thread subclass and start them up
-  }
+    public static void main(String[] args) {
+        // TODO: create two instances of your thread subclass and start them up
+
+        MessagePrinter printer1 = new MessagePrinter("roses are red", 10, 3000);
+        printer1.start();
+
+        MessagePrinter printer2 = new MessagePrinter("violets are blue", 100, 200);
+        printer2.start();
+
+        MessagePrinter printer3 = new MessagePrinter("sugar is sweet", 1000);
+        printer3.start();
+
+        MessagePrinter printer4 = new MessagePrinter("and so are you", 16, 2000);
+        printer4.start();
+
+
+
+
+    }
 }
